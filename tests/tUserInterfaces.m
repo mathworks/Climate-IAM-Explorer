ef636c2134a6a2f8c5686c57ee335cc144dd68f3
Edu Benet Cerda <ebenetce@mathworks.com>
2024-06-17 14:54:05 +0100

    added test for base app

---
+++ tests/tUserInterfaces.m
@@ -0,0 +1,23 @@
+classdef tUserInterfaces < matlab.uitest.TestCase
+    properties
+        App
+    end
+    methods (TestClassSetup)
+        function loadApp(tc)
+            tc.App = IAMDataExplorer;
+        end
+    end
+
+    methods (Test)
+        % Test methods
+
+        function tGuestLogin(testCase)
+            testCase.press(testCase.App.GuestLoginButton)
+            testCase.verifyFalse(isvalid(testCase.App))
+            h = findall(0,'Type','figure', 'Name','IAM Explorer');
+            testCase.verifyTrue(isvalid(h));
+            delete(h);
+        end
+    end
+
+end
