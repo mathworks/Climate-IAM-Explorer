62142615c0f3ac5bf0b4911a53ba6bb18f049c4f
Edu Benet Cerda <ebenetce@mathworks.com>
2024-06-18 14:23:14 +0100

    updated packaging + bumped to 1.4.0

--- buildfile.m
+++ buildfile.m
@@ -25,12 +25,18 @@ end
 function archiveTask(~)
 % Create ZIP file
 opts = matlab.addons.toolbox.ToolboxOptions('tbx', '5ed2c037-ac9c-4feb-8032-469d0a2b0285');
+
 opts.AuthorName = "Eduard Benet";
 opts.AuthorCompany = "MathWorks";
 opts.AuthorEmail = "ebenetce@mathworks.com";
 opts.Description = "This repository contains a set of tools to allow users explore integrated assesmemnt models and some examples showcasing how one can use this information in conjuntion to financial applications. The models accessible by the tool include the different datasets hosted by the IIASA Energy program (ENE), but it is extensible to your own custom models.";
 opts.Summary = "App to explore Integrated Assessment Model results";
 opts.AppGalleryFiles = "tbx/IAMDataExplorer/app/IAMDataExplorer.mlapp";
-opts.OutputFile = 'IAMDataExplorer.mltbx';
 opts.ToolboxGettingStartedGuide = "tbx/doc/GettingStarted.mlx";
+opts.ToolboxName = "Climate IAM Explorer";
+opts.ToolboxVersion = "1.4.0";
+opts.ToolboxMatlabPath = ["tbx/IAMDataExplorer", "tbx/IAMDataExplorer/app", "tbx/ISMIPExplorer"];
+opts.OutputFile = 'IAMDataExplorer.mltbx';
+
+matlab.addons.toolbox.packageToolbox(opts)
 end
